065016f18e35a6089216a015d5267995063da3f1
Qing Liu <Qing.Liu@du.edu>
2016-03-03 17:07:44 -0700

    change wsgi settings for multiple django sites

--- aws_admin/aws_admin/wsgi.py
+++ aws_admin/aws_admin/wsgi.py
@@ -7,8 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aws_admin.settings")
+import os, sys
+sys.path.append('/django-apps/aws_admin/aws_admin/')
+# Use this for single Django site in a single mod_wsgi process
+#os.environ.setdefault("PYTHON_EGG_CACHE", "/django-apps/aws_admin/aws_admin/egg_cache")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aws_admin.settings")
+# Use this for multiple Django site in a single mod_wsgi process
+os.environ["PYTHON_EGG_CACHE"] = "/django-apps/aws_admin/aws_admin/egg_cache")
+os.environ["DJANGO_SETTINGS_MODULE"] = "aws_admin.settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
