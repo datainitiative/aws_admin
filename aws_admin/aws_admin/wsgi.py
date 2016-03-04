2790d32e3d0ecfb65891ef099b5a2ad37d8d5234
Qing Liu <Qing.Liu@du.edu>
2016-03-04 14:06:10 -0700

    typo fix

--- aws_admin/aws_admin/wsgi.py
+++ aws_admin/aws_admin/wsgi.py
@@ -13,7 +13,7 @@ sys.path.append('/django-apps/aws_admin/aws_admin/')
 #os.environ.setdefault("PYTHON_EGG_CACHE", "/django-apps/aws_admin/aws_admin/egg_cache")
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aws_admin.settings")
 # Use this for multiple Django site in a single mod_wsgi process
-os.environ["PYTHON_EGG_CACHE"] = "/django-apps/aws_admin/aws_admin/egg_cache")
+os.environ["PYTHON_EGG_CACHE"] = "/django-apps/aws_admin/aws_admin/egg_cache"
 os.environ["DJANGO_SETTINGS_MODULE"] = "aws_admin.settings"
 
 from django.core.wsgi import get_wsgi_application
